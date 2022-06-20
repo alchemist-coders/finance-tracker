a479fe8c7bb8662736c578be655cf4e2c8a9f994
Soumyajit Das <mepioneiro@gmail.com>
2022-06-20 21:39:50 +0530

    fix navbar export

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,22 +1,21 @@
-import React from 'react'
+import React from "react";
+import Link from "next/link";
 
-export const Navbar = () => {
+const Navbar = () => {
   return (
-    
-    <nav className=' flex justify-between items-center px-8 py-4 border-b-2 shadow-lg  '>
-        <h1 className=' text-3xl m-2 font-semibold '   >Finance Tracker </h1>
-        <ul className='flex justify-around text-xl gap-4   '>
-            <li>
-                Dashboard
-            </li>
-            <li>
-                Transactions
-            </li>
-            <li>
-                Profile
-            </li>
-        </ul>
+    <nav className=" flex justify-between items-center px-8 py-4 border-b-2 shadow-lg  ">
+      <h1 className=" text-3xl m-2 font-semibold ">Finance Tracker </h1>
+      <ul className="flex justify-around text-xl gap-4   ">
+        <li>
+          <Link href="/dashboard">
+            <a>Dashboard</a>
+          </Link>
+        </li>
+        <li>Transactions</li>
+        <li>Profile</li>
+      </ul>
     </nav>
-    
-  )
-}
+  );
+};
+
+export default Navbar;
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,10 +1,9 @@
-import { Navbar } from "../components/Navbar";
+import Navbar from "../components/Navbar";
+
 export default function () {
-  return <>
-  <Navbar/>   
-
-
-
-
-  </>;
+  return (
+    <>
+      <Navbar />
+    </>
+  );
 }
