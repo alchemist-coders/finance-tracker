aac686990f2c3da31b0cb9f71f39a70cc6b6007f
Soumyajit Das <mepioneiro@gmail.com>
2022-06-20 21:54:56 +0530

    navbar animation

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -5,14 +5,14 @@ const Navbar = () => {
   return (
     <nav className=" flex justify-between items-center px-8 py-4 border-b-2 shadow-lg  ">
       <h1 className=" text-3xl m-2 font-semibold ">Finance Tracker </h1>
-      <ul className="flex justify-around text-xl gap-4   ">
-        <li>
+      <ul className="flex justify-around text-xl gap-4">
+        <li className="px-2 py-1 scale-100 hover:scale-105 rounded-lg border-b border-r border-transparent hover:border-black transition">
           <Link href="/dashboard">
             <a>Dashboard</a>
           </Link>
         </li>
-        <li>Transactions</li>
-        <li>Profile</li>
+        <li className="px-2 py-1 scale-100 hover:scale-105 rounded-lg border-b border-r border-transparent hover:border-black transition">Transactions</li>
+        <li className="px-2 py-1 scale-100 hover:scale-105 rounded-lg border-b border-r border-transparent hover:border-black transition">Profile</li>
       </ul>
     </nav>
   );
