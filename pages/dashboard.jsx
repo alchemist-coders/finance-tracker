46e579798c4629e3f7830dcb960f499190bf45c6
Debosmit Choudhury <debosmitc@gmail.com>
2022-06-27 17:06:34 +0530

    Added fixes

--- pages/dashboard.jsx
+++ pages/dashboard.jsx
@@ -11,22 +11,22 @@ const dashboard = () => {
         <p className="m-8 text-2xl">Total Investments: 22k</p>
 
         <section className="flex justify-around items-center">
-          <div className=" bg-blue-500 hover:text-white hover:bg-blue-700 text-white active:bg-blue-400 flex flex-col items-center border-2 border-gray-300 rounded-xl shadow-lg hover:shadow-2xl scale-100 hover:scale-105 w-1/5 px-8 py-4 gap-2 font-medium transition">
+          <div className=" bg-blue-500 hover:text-white hover:bg-blue-700 text-white active:bg-blue-400 flex flex-col items-center border-2 border-gray-300 rounded-xl shadow-lg cursor-pointer hover:shadow-2xl scale-100 hover:scale-105 w-1/5 px-8 py-4 gap-2 font-medium transition">
             <h3>Mutual Funds</h3>
             <p className="text-2xl">20k</p>
           </div>
 
-          <div  className=" bg-green-500 hover:text-white hover:bg-green-600 text-white active:bg-green-400 flex flex-col items-center border-2 border-gray-300 rounded-xl shadow-lg hover:shadow-2xl scale-100 hover:scale-105 w-1/5 px-8 py-4 gap-2 font-medium transition">
+          <div  className=" bg-green-500 hover:text-white hover:bg-green-600 text-white active:bg-green-400 flex flex-col items-center border-2 border-gray-300 rounded-xl shadow-lg cursor-pointer hover:shadow-2xl scale-100 hover:scale-105 w-1/5 px-8 py-4 gap-2 font-medium transition">
             <h3>Stocks</h3>
             <p className="text-2xl">2k</p>
           </div>
 
-          <div className=" bg-purple-800 hover:text-white hover:bg-purple-900 text-white active:bg-purple-600 flex flex-col items-center border-2 border-gray-300 rounded-xl shadow-lg hover:shadow-2xl scale-100 hover:scale-105 w-1/5 px-8 py-4 gap-2 font-medium transition">
+          <div className=" bg-purple-800 hover:text-white hover:bg-purple-900 text-white active:bg-purple-600 flex flex-col items-center border-2 border-gray-300 rounded-xl shadow-lg cursor-pointer hover:shadow-2xl scale-100 hover:scale-105 w-1/5 px-8 py-4 gap-2 font-medium transition">
             <h3>Crypto</h3>
             <p className="text-2xl">-</p>
           </div>
 
-          <div className="bg-yellow-500 hover:text-white hover:bg-yellow-600 text-white active:bg-amber-400 flex flex-col items-center border-2 border-gray-300 rounded-xl shadow-lg hover:shadow-2xl scale-100 hover:scale-105 w-1/5 px-8 py-4 gap-2 font-medium transition">
+          <div className="bg-yellow-500 hover:text-white hover:bg-yellow-600 text-white active:bg-amber-400 flex flex-col items-center border-2 border-gray-300 rounded-xl shadow-lg cursor-pointer hover:shadow-2xl scale-100 hover:scale-105 w-1/5 px-8 py-4 gap-2 font-medium transition">
             <h3>Gold</h3>
             <p className="text-2xl">-</p>
           </div>
